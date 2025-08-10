af31f9070eddf35a5ecb6ea06820b05571f4b5d0
Kalaiselvi-21 <nytowlkalai@gmail.com>
2025-08-10 11:26:44 +0530

    Updated db.js

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'sql12.freesqldatabase.com',
   port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'Negu@rt21',
-  database: process.env.DB_NAME || 'school_db',
+  user: process.env.DB_USER || 'sql12794397',
+  password: process.env.DB_PASSWORD || 'FvglTqaAUl',
+  database: process.env.DB_NAME || 'sql12794397',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
